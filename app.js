111627d5cdb3624e84afac4ad86beb1de4095852
tiagogdella <tigdella@gmail.com>
2026-03-26 17:21:20 -0300

    Ajustes dia 26/03/26

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ function iniciarJogo() {
 }
 
 function adicionarJogador() {
+    const imagem = document.getElementById('imagem-sid');
+    if(imagem){
+        imagem.style.display = 'none';
+    }
+    
     const input = document.getElementById('nomeJogador');
     const nomeJogador = input.value.trim();
 
@@ -47,4 +52,73 @@ function adicionarJogador() {
     });
 
     input.value = '';
+}
+
+function fazerPix() {
+    abrirBannerPix();
+
+    const jogador1 = document.getElementById('nome1');
+    const jogador2 = document.getElementById('nome2');
+    const valorPix = document.getElementById('valor');
+
+    if (!jogador1 || !jogador2) {
+        console.error('Jogador não encontrado');
+        return;
+    };
+
+    if (jogador1.dinheiro < valor) {
+        abriModalSemSaldo(nome1, nome2, valor);
+        return;
+    };
+
+    jogador1.dinheiro -= valorPix.value;
+    jogador2.dinheiro += valorPix.value;
+
+    atualizarDinheiro(jogador1.textContent);
+    atualizarDinheiro(jogador2.textContent);
+}
+
+function abrirBannerPix() {
+    const selectDe = document.getElementById('nome1');
+    const selectPara = document.getElementById('nome2');
+
+    selectDe.innerHTML = '';
+    selectPara.innerHTML = '';
+    
+    jogadores.forEach(jogador => { 
+        const option1 = document.createElement('option');
+        option1.value = jogador.nome;
+        option1.textContent = jogador.nome;
+        
+        const option2 = document.createElement('option');
+        option2.value = jogador.nome;
+        option2.textContent = jogador.nome;
+
+        selectDe.appendChild(option1);
+        selectPara.appendChild(option2);
+    });
+
+    const banner = document.getElementById('bannerPix');
+    banner.style.display = 'block';
+
+}
+
+function fecharBannerPix() {
+    const banner = document.getElementById('bannerPix');
+    banner.style.display = 'none';
+}
+
+function abriModalSemSaldo(nome1, nome2, valor) {
+    document.getElementById('meuBanner').style.display = 'block';
+}
+function fecharModalSemSaldo() {
+    document.getElementById('meuBanner').style.display = 'none';
+}
+
+function atualizarDinheiro(nome) {
+    const dinheiroElement = document.getElementById(`dinheiro-${nome}`);
+    if (dinheiroElement) {
+        const jogador = jogadores.find(jogador => jogador.nome === nome);
+        dinheiroElement.textContent = `R$ ${jogador.dinheiro}`;
+    };
 }
